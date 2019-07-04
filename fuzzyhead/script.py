52a14f9b13ecb96645da29d114dac08c15ea5752
brain2998 <an.sereb96@gmail.com>
2019-07-04 17:00:48 +0300

    parse output

--- fuzzyhead/script.py
+++ fuzzyhead/script.py
@@ -7,17 +7,19 @@ from celery.task.control import inspect
 import os
 import requests
 import time
+import json
 
 def start_fuzzing(fuzzer_type, dict_path, divide_number, target_ip):
     if fuzzer_type=='patator':
         fuzzer_container = "registry:443/patator:latest"
         fuzzer_start = "python2 -W ignore patator.py"
         fuzzer_options = "ssh_login host="+target_ip+" user=victim password=FILE0 0=passwords.txt -x ignore:mesg='Authentication failed.'"
-
+        parse_func = parsePatator
     if fuzzer_type == "dirsearch":
         fuzzer_container = "registry:443/dirsearch:latest"
         fuzzer_start = "python3 -W ignore dirsearch.py"
         fuzzer_options = "--url "+target_ip+" -e html --wordlist=wordlist.txt --simple-report=/dirsearch/result.txt"
+        parse_func = parseDirsearch
     upload_dict_url = "http://backend:5000/uploader"
     remove_dict_url = "http://backend:5000/remover"
 
@@ -66,4 +68,27 @@ def start_fuzzing(fuzzer_type, dict_path, divide_number, target_ip):
                print(round(100 - l/a * 100))
 
     os.remove(dict_path)
-    return output
+    return parse_func(output)
+
+def parsePatator(output):
+    strings=output.split('\\n')
+    match=strings[4].split('|')
+    stats=strings[-2]
+    valueIndex=stats.index('Size: ', )+6
+    hits=stats[valueIndex:valueIndex+1]
+    done=stats[valueIndex+2:valueIndex+3]
+    skip=stats[valueIndex+4:valueIndex+5]
+    fail=stats[valueIndex+6:valueIndex+7]
+    size=stats[valueIndex+8:valueIndex+9]
+    time=stats[stats.index('Time: ')+6:]
+    return json.dumps({'match':match[1], 'hits':hits, 'done':done, 
+    'skip':skip, 'fail':fail, 'size':size, 'time':time})
+
+def parseDirsearch(output):
+    strings=output.split('\n')
+    if '' in strings:
+        strings.remove('')
+    result=[]
+    for s in strings:
+        result.append(s[s.rindex('/')+1:])
+    return json.dumps({'match':result})
